f96cf366f0d70b6f71d2ce0a7997759018c1e1b7
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-06-05 11:24:31 +0200

    ASP.NET With Dapper inital commit

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,38 @@
+using CompanyEmployees.Extensions;
+using Contracts;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers()
+	.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if (app.Environment.IsProduction())
+	app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+	ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
